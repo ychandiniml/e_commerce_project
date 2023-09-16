cbd125b15308f1a1cd6083e6a81de7960644dad8
chandini <ychandiniml@gmail.com>
2023-09-16 23:46:49 +0530

    added router and login pageI

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -7,12 +7,60 @@ import {
   Cart,
   About,
   Register,
-  Login,
+  Login, 
   Checkout,
   Orders,
 } from './pages';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: 'products',
+        element: <Products />,
+      },
+      {
+        path: 'products/:id',
+        element: <SingleProduct />,
+      },
+      {
+        path: 'cart',
+        element: <Cart />,
+      },
+      { path: 'about', element: <About /> },
+      {
+        path: 'checkout',
+        element: <Checkout />,
+      },
+      {
+        path: 'orders',
+        element: <Orders />,
+      },
+    ],
+  },
+  {
+    path: '/login',
+    element: <Login />,
+    errorElement: <Error />,
+  },
+  {
+    path: '/register',
+    element: <Register />,
+    errorElement: <Error />,
+  },
+]);
 
 const App = () => {
-  return <h1 className='text-7xl font-bold underline'>Tailwind project</h1>;
+  return <RouterProvider router={router} />;
 };
 export default App;
+
+
