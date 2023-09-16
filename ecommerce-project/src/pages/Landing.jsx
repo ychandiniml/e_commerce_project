d279fe56906757b193a0089c51464b57dbd0a7ef
chandini <ychandiniml@gmail.com>
2023-09-17 01:25:38 +0530

    added featured products and productsGrid component

---
+++ ecommerce-project/src/pages/Landing.jsx
@@ -0,0 +1,20 @@
+import { Hero } from '../components';
+
+import { customFetch } from '../utils';
+const url = '/products?featured=true';
+
+export const loader = async () => {
+  const response = await customFetch(url);
+  console.log(response);
+  const products = response.data.data;
+  return { products };
+};
+
+const Landing = () => {
+  return (
+    <>
+      <Hero />
+    </>
+  );
+};
+export default Landing;
