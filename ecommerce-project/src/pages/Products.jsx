cece2112491a946ac7daf06f7d06094f7b7bfc2d
chandini <ychandiniml@gmail.com>
2023-09-17 01:32:51 +0530

    added products and SingleProduct page

---
+++ ecommerce-project/src/pages/Products.jsx
@@ -0,0 +1,22 @@
+import { Filters, PaginationContainer, ProductsContainer } from '../components';
+import { customFetch } from '../utils';
+
+const url = '/products';
+export const loader = async ({ request }) => {
+  const response = await customFetch(url);
+
+  const products = response.data.data;
+  const meta = response.data.meta;
+  return { products, meta };
+};
+
+const Products = () => {
+  return (
+    <>
+      <Filters />
+      <ProductsContainer />
+      <PaginationContainer />
+    </>
+  );
+};
+export default Products;
