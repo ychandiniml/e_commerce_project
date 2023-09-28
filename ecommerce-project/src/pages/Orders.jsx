61f5ae44a703ca8ebdaa840967247d19180ee5f1
chandini <ychandiniml@gmail.com>
2023-09-28 21:26:29 +0530

    added orders,checkoutForm and headers

--- ecommerce-project/src/pages/Orders.jsx
+++ ecommerce-project/src/pages/Orders.jsx
@@ -1,52 +1,69 @@
 import { redirect, useLoaderData } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { customFetch } from '../utils';
-import { OrdersList, PaginationContainer, SectionTitle } from '../components';
+import {
+  OrdersList,
+  ComplexPaginationContainer,
+  SectionTitle,
+} from '../components';
+
+const ordersQuery = (params, user) => {
+  return {
+    queryKey: [
+      'orders',
+      user.username,
+      params.page ? parseInt(params.page) : 1,
+    ],
+    queryFn: () =>
+      customFetch.get('/orders', {
+        params,
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      }),
+  };
+};
 
 export const loader =
-  (store) =>
+  (store, queryClient) =>
   async ({ request }) => {
     const user = store.getState().userState.user;
 
     if (!user) {
-      toast.warn('You must be logged in to view orders');
+      toast.warn('You must logged in to view orders');
       return redirect('/login');
     }
     const params = Object.fromEntries([
       ...new URL(request.url).searchParams.entries(),
     ]);
     try {
-      const response = await customFetch.get('/orders', {
-        params,
-        headers: {
-          Authorization: `Bearer ${user.token}`,
-        },
-      });
+      const response = await queryClient.ensureQueryData(
+        ordersQuery(params, user)
+      );
 
       return { orders: response.data.data, meta: response.data.meta };
     } catch (error) {
       console.log(error);
       const errorMessage =
         error?.response?.data?.error?.message ||
-        'there was an error accessing your orders';
-
+        'there was an error placing your order';
       toast.error(errorMessage);
       if (error?.response?.status === 401 || 403) return redirect('/login');
-
       return null;
     }
   };
+
 const Orders = () => {
-    const { meta } = useLoaderData();
-    if (meta.pagination.total < 1) {
-      return <SectionTitle text='Please make an order' />;
-    }
-    return (
-      <>
-        <SectionTitle text='Your Orders' />
-        <OrdersList />
-        <PaginationContainer />
-      </>
-    );
+  const { meta } = useLoaderData();
+  if (meta.pagination.total < 1) {
+    return <SectionTitle text='please make an order' />;
+  }
+  return (
+    <>
+      <SectionTitle text='Your Orders' />
+      <OrdersList />
+      <ComplexPaginationContainer />
+    </>
+  );
 };
 export default Orders;
